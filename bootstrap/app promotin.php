1d72f746d153154089c8bffcc2e372b74d12374c
Sahil <sahilgodse1998@gmail.com>
2018-09-16 15:06:16 +0530

    app banner

---
+++ bootstrap/app promotin.php
@@ -0,0 +1,21 @@
+
+<div class="container-fluid" style="padding: 0; border: 1px solid gray;">
+    
+    <div class="col-md-2 col-xs-12" style=" height: auto; float: left; padding: 0; text-align: center;">
+        <div class="lead" style="font-weight: bold; font-size: 24px;">Now health care <br> in your pocket</div>
+        <img onclick="javascript:location.href='https://itunes.apple.com/in/app/';" style="margin-top: 10px; cursor: pointer; max-width: 100%; height: auto;" src="image/app-store.jpg"/>
+        <img onclick="javascript:location.href='https://play.google.com/store/apps/';" src="image/play-store.jpg" style="cursor: pointer; max-width: 100%; cursor: pointer; height: auto;"/>
+        <or style="font-size: 25px; text-align: center;">OR</or>
+        <nav class="navbar navbar-light bg-light">
+                <form class="form-inline">
+                    <input class="form-control mr-sm-2" type="text" placeholder="Enter mobile number" aria-label="Search" style=""><br><br>
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Submit</button>
+                </form>
+        </nav>
+    </div>
+    <img src="image/app.png" style="max-width: 100%; height: auto; float: left;"/> 
+</div>
+
+
+        
+  
