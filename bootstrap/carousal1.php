a6821bb75fbf82810563d8b575fb70208fb83c03
Sahil <sahilgodse1998@gmail.com>
2018-09-16 13:31:54 +0530

    creating carousel

---
+++ bootstrap/carousal1.php
@@ -0,0 +1,91 @@
+
+    <div id="myCarousel0" style="height: auto;" class="carousel slide" data-ride="carousel">
+        <!-- Indicators -->
+        
+        <!-- Wrapper for slides -->
+        <h1 style="text-align:center;">We're Right There With You</h1>
+        <div class="carousel-inner" style="height: auto;" role="listbox">
+          <div class="item active">
+            <img src="image/c1-i1.jpg" style="max-width: 100%; height: auto; margin: auto;"/>
+            
+            <img src="image/AHA_Cert.jpg" style="max-width: 100%; height: auto; position: absolute; left: 1000px; top: 5px; opacity: 0.8;">
+            <div class="row align-items-center justify-content-center" style="position: absolute; left: 500px; top: 150px; background: gray; color: #000; opacity: 0.7; width:350px; height: 100px; font-size: 16px; font-weight: bold; box-sizing: border-box; padding: 25px;">
+                    
+                <input type="button" value="Make an Appointment" style="color: white; width: 200px; height: 50px; border: none; background: #DF5C43;"  data-toggle="modal" data-target="#myModal1">
+                <br>Call (888) 352-7874  
+                
+            </div>
+
+            <!-- Modal -->
+            <div id="myModal1" class="modal fade" role="dialog">
+                    <div class="modal-dialog">
+
+                            <!-- Modal content-->
+                            <div class="modal-content">
+                                    <div class="modal-header" style="background: #00A950;">
+                                            <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                            <h4 class="modal-title">Name</h4>
+                                    </div>
+                                    <div class="modal-body" style="text-align: center;">  Book an appointment  <br><br>
+                                            <div class="row" style="background: #ebeeef;">
+                                                <div class="form-group">
+                                                        <label for="exampleFormControlSelect1">Select your city</label>
+                                                        <select class="form-control" id="exampleFormControlSelect1">
+                                                                <option>1</option>
+                                                                <option>2</option>
+                                                                <option>3</option>
+                                                                <option>4</option>
+                                                                <option>5</option>
+                                                        </select>
+                                                </div>
+                                                <div class="form-group">
+                                                        <label for="exampleFormControlSelect1">Select your hospital</label>
+                                                        <select class="form-control" id="exampleFormControlSelect1">
+                                                                <option>1</option>
+                                                                <option>2</option>
+                                                                <option>3</option>
+                                                                <option>4</option>
+                                                                <option>5</option>
+                                                        </select>
+                                                </div>
+                                            </div><br>
+                                            <button type="button" class="btn btn-success">Continue</button>
+                                    </div>
+                                    <div class="modal-footer">
+                                            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                    </div>
+                            </div>
+
+                    </div>
+                </div>
+
+          </div>
+          <div class="item">
+            <img src="image/c1-i2.jpg" style="max-width: 100%; height: auto; margin: auto;"/>
+            <div class="row align-items-center justify-content-center" style="position: absolute; left: 400px; top: -12px;">
+                <img src="image/DNV_Stroke_Cert.jpg" style="max-width: 100%; height: auto;">
+            </div>
+          </div>
+      
+          <div class="item">
+            <img src="image/c1-i3.jpg" style="max-width: 100%; height: auto; margin: auto;"/>
+          </div>
+      
+          <div class="item">
+            <img src="image/c1-i4.jpg" style="max-width: 100%; height: auto; margin: auto;"/>
+          </div>
+          <div class="item">
+            <img src="image/c1-i5.jpg" style="max-width: 100%; height: auto; margin: auto;"/>
+          </div>
+        </div>
+      
+        <!-- Left and right controls -->
+        <a class="left carousel-control" href="#myCarousel0" role="button" data-slide="prev" style="background: rgba(251, 247, 243, 1); width: 12%;">
+          <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" href="#myCarousel0" role="button" data-slide="next" style="background: rgba(251, 247, 243, 1); width: 12%;">
+          <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+    </div>
