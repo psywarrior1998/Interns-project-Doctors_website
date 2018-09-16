36cff4b901d9c3d4a5dd0caa2e0146810ed0df05
Sahil <sahilgodse1998@gmail.com>
2018-09-16 13:25:37 +0530

    adding footer to the website

---
+++ bootstrap/footer.php
@@ -0,0 +1,69 @@
+
+<div class="container-fluid" id="footer" style="border-top:10px solid #00A950; background: white; height: 350px; box-sizing: border-box; padding: 0 60px;">
+    <div class="row">
+        <div class="col-md-3 col-xs-12 col-sm-3" style="box-sizing: border-box; padding: 10px;">
+
+                <div class="row" style="border-bottom: 1px solid black; box-sizing: border-box; padding: 10px;"><b style="font-size: 16px;">PATIENT CARE & SERVICES</b></div>
+                <br><a href="admission.php"><i style="font-size: 12px; padding: 0; margin: 0; width: 20px; height: 20px;"  class="fa fa-chevron-right" aria-hidden="true"></i>
+                    Admission Process</a><br><br>
+                <a href="discharge.php"><i style="font-size: 12px; padding: 0; margin: 0; width: 20px; height: 20px;" class="fa fa-chevron-right" aria-hidden="true"></i>
+                    Discharge Process</a><br><br>
+                <a href="guidelines for new.php"><i style="font-size: 12px; padding: 0; margin: 0; width: 20px; height: 20px;" class="fa fa-chevron-right" aria-hidden="true"></i>
+                    Guidelines For Visitors</a><br><br>
+                <a href="clinical outcomes.php"><i style="font-size: 12px; padding: 0; margin: 0; width: 20px; height: 20px;" class="fa fa-chevron-right" aria-hidden="true"></i>
+                    Clinical Outcomes</a>
+
+        </div>
+        <div class="col-md-3 col-xs-12 col-sm-3" style="box-sizing: border-box; padding: 10px;">
+
+                <div class="row" style="border-bottom: 1px solid black; box-sizing: border-box; padding: 10px;"><b style="font-size: 16px;">ABOUT US</b></div>
+                <br><a href="vm values.php"><i style="font-size: 12px; padding: 0; margin: 0; width: 20px; height: 20px;"  class="fa fa-chevron-right" aria-hidden="true"></i>
+                    Vision</a><br><br>
+                <a href="vm values.php"><i style="font-size: 12px; padding: 0; margin: 0; width: 20px; height: 20px;" class="fa fa-chevron-right" aria-hidden="true"></i>
+                    Mission</a><br><br>
+                <a href="vm values.php"><i style="font-size: 12px; padding: 0; margin: 0; width: 20px; height: 20px;" class="fa fa-chevron-right" aria-hidden="true"></i>
+                    Values</a><br><br>
+
+        </div>
+        <div class="col-md-3 col-xs-12 col-sm-3" style="box-sizing: border-box; padding: 10px;">
+                <div class="row" style="border-bottom: 1px solid black; box-sizing: border-box; padding: 10px;"><b style="font-size: 16px;">DISEASES AND CONDITIONS</b></div>
+                <br><a href="high bp.php"><i style="font-size: 12px; padding: 0; margin: 0; width: 20px; height: 20px;"  class="fa fa-chevron-right" aria-hidden="true"></i>
+                    High Blood Pressure</a><br><br>
+                <a href="diabetes.php"><i style="font-size: 12px; padding: 0; margin: 0; width: 20px; height: 20px;" class="fa fa-chevron-right" aria-hidden="true"></i>
+                    Diabetes</a><br><br>
+                <a href="Osteoporosis.php"><i style="font-size: 12px; padding: 0; margin: 0; width: 20px; height: 20px;" class="fa fa-chevron-right" aria-hidden="true"></i>
+                    Osteoporosis</a><br><br>
+                <a href="heartfail.php"><i style="font-size: 12px; padding: 0; margin: 0; width: 20px; height: 20px;" class="fa fa-chevron-right" aria-hidden="true"></i>
+                    Heart failure</a><br><br>
+    
+        </div>
+        <div class="col-md-3 col-xs-12 col-sm-3" style="box-sizing: border-box; padding: 10px;">
+            <div class="row" style="border-bottom: 1px solid black; box-sizing: border-box; padding: 10px;"><b style="font-size: 16px;">STAY IN TOUCH</b></div>
+            <div class="row" style="box-sizing: border-box; padding-top: 20px; margin-bottom: 15px;">
+                    Tower A, Unitech Business Park,<br>
+                    Block - F, South City 1, Sector - 41,<br>
+                    Bristol, UK - 122001
+            </div>
+            <div class="row">
+                    <a href="https://www.facebook.com/" class="fa fa-facebook s_icons mt-35 ml-2 mb-35" style="background: #3B5998; color: white"></a>
+                    <a href="https://www.twitter.com/" class="fa fa-twitter s_icons mt-35 ml-2 mb-35" style="background: #55ACEE; color: white"></a>
+                    <a href="https://plus.google.com/discover" class="fa fa-google s_icons mt-35 ml-2 mb-35" style="background: #dd4b39; color: white"></a>
+                    <a href="https://www.linkedin.com/uas/login" class="fa fa-linkedin s_icons mt-35 ml-2 mb-35" style="background: #007bb5; color: white"></a>
+                    <a href="https://www.instagram.com/webdev.7/" class="fa fa-instagram s_icons mt-35 ml-2 mb-35" style="background: #125688; color: white"></a>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-4 col-xs-12" style="background: white; height: auto;">
+                    <p class=" .small text-md-left text-xs-center">Privacy Policy|Terms & Conditions|Sitemap
+                    &copy; 2018 Doctors Healthcare. All Rights Reserved.                 
+                    </p>
+            </div>
+            <div class="col-md-3 col-xs-12" style="background: white; height: auto; float: right;">
+                    <img src="image/logo 2.jpg" class="img-fluid" style="height: auto; max-width: 100%;"/>
+            </div>
+        </div>
+    </div>
+    
+</div>
