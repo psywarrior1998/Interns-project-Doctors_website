6dffa5a54f5f1a64e70bfa71869ecddaaa13bb4b
Sahil <sahilgodse1998@gmail.com>
2018-09-16 15:07:37 +0530

    patients speak

---
+++ bootstrap/patients speak.php
@@ -0,0 +1,209 @@
+
+
+        <div class="container-fluid" style="height: auto; background: white;">
+                <div class="container" style="height: auto; background: white;">
+                    
+                    <div class="lead" style="text-align: center;"><i class="fa fa-comments-o" aria-hidden="true"></i>&nbsp;Patient's Speak</div>
+                    
+                        <div id="myCarousel1" class="carousel slide" data-ride="carousel" style="height: 260px;">
+                                    <!-- Indicators not needed here-->
+                                    <!-- Wrapper for slides -->
+                                    <div class="carousel-inner" role="listbox" style="height: 260px;">
+                                        <div class="item active" style="height: 260px;">
+                                            <div class="row">
+                                                <div class="col-sm-6" style="border-right: 1px solid #C2C3C4;">
+                                                    <div class="card">
+                                                            <div class="card-body">
+                                                              <h5 class="card-title">Mr. Venkatesh</h5>
+                                                              <p class="card-text">
+                                                                    <p><i class="fa fa-quote-left" style="color: #FF0000" aria-hidden="true"></i>
+                                                                    </p>
+                                                                    <p>I would like to accord my appreciation to 
+                                                                       Dr. Sudarshan & team of emergency section, 
+                                                                       for having received my known person Mrs. Sarojamma 
+                                                                       yesterday night for her cardiac issue, 
+                                                                       very grateful to the team.
+                                                                    </p>
+                                                                    <i class="fa fa-quote-right" aria-hidden="true"></i>
+                                                              </p>
+                                                              
+                                                            </div>
+                                                    </div>
+                                                </div>
+                                                <div class="col-sm-6">
+                                                    <div class="card">
+                                                        <div class="card-body">
+                                                                  <h5 class="card-title">Mr. Parmindar, Mohali</h5>
+                                                                  <p class="card-text">
+                                                                        <p><i class="fa fa-quote-left" style="color: #FF0000" aria-hidden="true"></i>
+                                                                        </p>
+                                                                        <p>I would like to accord my appreciation to 
+                                                                           Dr. Sudarshan & team of emergency section, 
+                                                                           for having received my known person Mrs. Sarojamma 
+                                                                           yesterday night for her cardiac issue, 
+                                                                           very grateful to the team.
+                                                                        </p>
+                                                                        <i class="fa fa-quote-right" aria-hidden="true"></i>
+                                                                  </p>
+                                                                  
+                                                        </div>
+                                                    </div>          
+                                                </div>
+                                            </div>
+                                        </div>
+                                  
+                                        <div class="item">
+                                                <div class="row">
+                                                        <div class="col-sm-6" style="border-right: 1px solid #C2C3C4;">
+                                                            <div class="card">
+                                                                    <div class="card-body">
+                                                                      <h5 class="card-title">Mr. Venkatesh</h5>
+                                                                      <p class="card-text">
+                                                                            <p><i class="fa fa-quote-left" style="color: #FF0000" aria-hidden="true"></i>
+                                                                            </p>
+                                                                            <p>I would like to accord my appreciation to 
+                                                                               Dr. Sudarshan & team of emergency section, 
+                                                                               for having received my known person Mrs. Sarojamma 
+                                                                               yesterday night for her cardiac issue, 
+                                                                               very grateful to the team.
+                                                                            </p>
+                                                                            <i class="fa fa-quote-right" aria-hidden="true"></i>
+                                                                      </p>
+                                                                  
+                                                                    </div>
+                                                            </div>
+                                                        </div>
+                                                        <div class="col-sm-6">
+                                                            <div class="card">
+                                                                <div class="card-body">
+                                                                          <h5 class="card-title">Mr. Parmindar,Mohali</h5>
+                                                                          <p class="card-text">
+                                                                                <p><i class="fa fa-quote-left" style="color: #FF0000" aria-hidden="true"></i>
+                                                                                </p>
+                                                                                <p>I would like to accord my appreciation to 
+                                                                                   Dr. Sudarshan & team of emergency section, 
+                                                                                   for having received my known person Mrs. Sarojamma 
+                                                                                   yesterday night for her cardiac issue, 
+                                                                                   very grateful to the team.
+                                                                                </p>
+                                                                                <i class="fa fa-quote-right" aria-hidden="true"></i>
+                                                                          </p>
+                                                                      
+                                                                </div>
+                                                            </div>          
+                                                        </div>
+                                                </div>
+                                            
+                                        </div>
+
+                                        <div class="item">
+                                                <div class="row">
+                                                        <div class="col-sm-6" style="border-right: 1px solid #C2C3C4;">
+                                                            <div class="card">
+                                                                    <div class="card-body">
+                                                                      <h5 class="card-title">Mr. Venkatesh</h5>
+                                                                      <p class="card-text">
+                                                                            <p><i class="fa fa-quote-left" style="color: #FF0000" aria-hidden="true"></i>
+                                                                            </p>
+                                                                            <p>I would like to accord my appreciation to 
+                                                                               Dr. Sudarshan & team of emergency section, 
+                                                                               for having received my known person Mrs. Sarojamma 
+                                                                               yesterday night for her cardiac issue, 
+                                                                               very grateful to the team.
+                                                                            </p>
+                                                                            <i class="fa fa-quote-right" aria-hidden="true"></i>
+                                                                      </p>
+                                                              
+                                                                    </div>
+                                                            </div>
+                                                        </div>
+                                                        <div class="col-sm-6">
+                                                            <div class="card">
+                                                                <div class="card-body">
+                                                                          <h5 class="card-title">Mr. Parmindar,Mohali</h5>
+                                                                          <p class="card-text">
+                                                                                <p><i class="fa fa-quote-left" style="color: #FF0000" aria-hidden="true"></i>
+                                                                                </p>
+                                                                                <p>I would like to accord my appreciation to 
+                                                                                   Dr. Sudarshan & team of emergency section, 
+                                                                                   for having received my known person Mrs. Sarojamma 
+                                                                                   yesterday night for her cardiac issue, 
+                                                                                   very grateful to the team.
+                                                                                </p>
+                                                                                <i class="fa fa-quote-right" aria-hidden="true"></i>
+                                                                          </p>
+                                                                         
+                                                                </div>
+                                                            </div>          
+                                                        </div>
+                                                </div>
+                                            
+                                        </div>
+
+                                        <div class="item">
+                                                <div class="row">
+                                                        <div class="col-sm-6" style="border-right: 1px solid #C2C3C4;">
+                                                            <div class="card">
+                                                                    <div class="card-body">
+                                                                      <h5 class="card-title">Mr. Venkatesh</h5>
+                                                                      <p class="card-text">
+                                                                            <p><i class="fa fa-quote-left" style="color: #FF0000" aria-hidden="true"></i>
+                                                                            </p>
+                                                                            <p>I would like to accord my appreciation to 
+                                                                               Dr. Sudarshan & team of emergency section, 
+                                                                               for having received my known person Mrs. Sarojamma 
+                                                                               yesterday night for her cardiac issue, 
+                                                                               very grateful to the team.
+                                                                            </p>
+                                                                            <i class="fa fa-quote-right" aria-hidden="true"></i>
+                                                                      </p>
+                                                                   
+                                                                    </div>
+                                                            </div>
+                                                        </div>
+                                                        <div class="col-sm-6">
+                                                            <div class="card">
+                                                                <div class="card-body">
+                                                                          <h5 class="card-title">Mr. Parmindar,Mohali</h5>
+                                                                          <p class="card-text">
+                                                                                <p><i class="fa fa-quote-left" style="color: #FF0000" aria-hidden="true"></i>
+                                                                                </p>
+                                                                                <p>I would like to accord my appreciation to 
+                                                                                   Dr. Sudarshan & team of emergency section, 
+                                                                                   for having received my known person Mrs. Sarojamma 
+                                                                                   yesterday night for her cardiac issue, 
+                                                                                   very grateful to the team.
+                                                                                </p>
+                                                                                <i class="fa fa-quote-right" aria-hidden="true"></i>
+                                                                          </p>
+                                                                  
+                                                                </div>
+                                                            </div>          
+                                                        </div>
+                                                </div>
+                                            
+                                        </div>
+                        
+                                        
+                                    </div>
+                                  
+                                    <!-- Left and right controls -->
+                                    <a class="left carousel-control" href="#myCarousel1" role="button" data-slide="prev" style="opacity: 0.2;">
+                                      <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                                      <span class="sr-only">Previous</span>
+                                    </a>
+                                    <a class="right carousel-control" href="#myCarousel1" role="button" data-slide="next" style="opacity: 0.2;">
+                                      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                                      <span class="sr-only">Next</span>
+                                    </a>
+                        </div>
+                    
+                    
+                </div>
+        </div>
+
+
+
+
+
+
