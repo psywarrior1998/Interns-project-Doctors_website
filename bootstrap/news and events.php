6c2946aa90fc59bbef089f091f7c44842d8a2b39
Sahil <sahilgodse1998@gmail.com>
2018-09-16 15:08:23 +0530

    patients speak

---
+++ bootstrap/news and events.php
@@ -0,0 +1,52 @@
+
+        <div class="container-fluid" style="height: auto; background: url('image/bg.jpg') no-repeat;">
+            <div class="container">
+                    <div class="col-md-4 col-xs-12" style="background: rgba(0,0,0,0.3); height: auto;">
+                            <div class="row" style="background: gainsboro; height: 100px; box-sizing: border-box; padding: 10px;
+                            font-size: 35px; color: white;">+ News & Events
+                            </div>                        
+                            <div class="card" style="width: 100%; background: aliceblue;">
+                                    <img class="card-img-top" src="image/news1.jpg" width="100%" height="100%" alt="Card image cap">
+                                    <div class="card-body">
+                                      <h5 class="card-title">BLS Training session for 400 NCC cadets<br>
+                                            Jul 21, 2018 to Jul 21, 2018</h5>
+                                      <p class="card-text">
+                                            BLS training session was organized for<br> students of NCC Camp at Mai Bhago<br> Polytechnic College, Amritsar. 400 NCC<br> Cadets participated in these …
+                                      </p>
+                                      <!--<a href="#" class="btn btn-primary">Go somewhere</a>-->
+                                      <a href="#" class="card-link"></a>
+    
+                                    </div>
+                            </div>
+    
+                    </div>
+                    <div class="col-md-4 col-xs-12" style="background: rgba(0,0,0,0.3); height: auto;">
+    
+                            <div class="card" style="width: 100%; background: aliceblue;">
+                                    <img class="card-img-top" src="image/news2.jpg" width="100%" height="100%" alt="Card image cap">
+                                    <div class="card-body">
+                                      <h5 class="card-title">Pro-Social Peer Moderator Program’s Module 8th - Gender Sensitization<br>
+                                            Jul 19, 2018 to Jul 17, 2018 </h5>
+                                      <p class="card-text">
+                                            The Pro-social Peer Moderator Program is <br> unique platform for students to learn <br>application based life-skills through the<br> medium of …
+                                      </p>
+                                      <!--<a href="#" class="btn btn-primary">Go somewhere</a>-->
+                                      <a href="#" class="card-link"></a>
+    
+                                    </div>
+                            </div>
+    
+                    </div>
+                    <div class="col-md-4 col-xs-12" style="background: rgba(0,0,0,0.3); height: auto;">
+                            <div class="row" style="background: gainsboro; height: 100px; box-sizing: border-box; padding: 10px;
+                            font-size: 35px; color: white;">+ Videos
+                            </div>   
+                            <div class="embed-responsive embed-responsive-16by9" style="margin-bottom: 10px;">
+                                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/-JzjDpwKflQ"></iframe>
+                            </div>
+                            <div class="embed-responsive embed-responsive-16by9">
+                                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/OoI57NeMwCc"></iframe>
+                            </div>
+                    </div>
+            </div>            
+        </div>
