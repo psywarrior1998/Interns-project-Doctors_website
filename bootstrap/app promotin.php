03020d06585eef93c900471e96550b05c086640a
Sahil <sahilgodse1998@gmail.com>
2018-09-16 16:48:37 +0530

    app banner modifn-2

--- bootstrap/app promotin.php
+++ bootstrap/app promotin.php
@@ -2,6 +2,7 @@
 <div class="container-fluid" style="padding: 0; border: 1px solid gray;">
     
     <div class="col-md-2 col-xs-12" style=" height: auto; float: left; padding: 0; text-align: center;">
+		<br><br>
         <div class="lead" style="font-weight: bold; font-size: 24px;">Now health care <br> in your pocket</div>
         <img onclick="javascript:location.href='https://itunes.apple.com/in/app/';" style="margin-top: 10px; cursor: pointer; max-width: 100%; height: auto;" src="image/app-store.jpg"/><br><br>
         <img onclick="javascript:location.href='https://play.google.com/store/apps/';" src="image/play-store.jpg" style="cursor: pointer; max-width: 100%; cursor: pointer; height: auto;"/>
